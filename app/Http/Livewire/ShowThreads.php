48b6e22051664049f5298986cd38679fdb0a959d
Luis Anillo <luis1104anillo@gmail.com>
2023-08-08 11:53:46 -0500

    creating the consults

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -3,11 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Thread;
 
 class ShowThreads extends Component
 {
     public function render()
     {
-        return view('livewire.show-threads');
+        //$categories = Category::paginate(10); // Pagina las categorías mostrando 10 por página
+        //$threads = Thread::latest()->paginate(15); // Pagina los hilos mostrando 15 por página
+
+        //withCount() nos permite contar
+
+        $categories = Category::get();
+        $threads = Thread::latest()->withCount('replies')->get();
+
+        return view('livewire.show-threads', [
+            'categories' => $categories,
+            'threads' => $threads,
+        ]);
     }
 }
