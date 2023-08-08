9b3951f304c49d1ad717521606632ff07fc80ed1
Luis Anillo <luis1104anillo@gmail.com>
2023-08-08 12:06:38 -0500

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\Reply;
 use App\Models\User;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create(['email' => 'luis@admin.com']);
+        User::factory()->create([
+            'name' => 'MSc. Luis',
+            'email' => 'luis@admin.com',
+            'password' => bcrypt('12345678')
+        ]);
         User::factory(9)->create();
 
-        Category::factory(10)->create();
+        Category::factory(10)
+            ->hasThreads(20)
+            ->create();
+
+        Reply::factory(400)->create();
     }
 }
