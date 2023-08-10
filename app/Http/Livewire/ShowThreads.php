4a8fb22dad0f59c9bc49aed9c7daec71307c7e90
Luis Anillo <luis1104anillo@gmail.com>
2023-08-09 19:58:05 -0500

    setting the logic to the filterByCategory function and the consult in the Search input

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -8,19 +8,35 @@ use App\Models\Thread;
 
 class ShowThreads extends Component
 {
+    public $search = '';
+    public $category = '';
+
+    public function filterByCategory($category) 
+    {
+        $this->category = $category;
+    }
+
     public function render()
     {
-        //$categories = Category::paginate(10); // Pagina las categorías mostrando 10 por página
-        //$threads = Thread::latest()->paginate(15); // Pagina los hilos mostrando 15 por página
-
         //withCount() nos permite contar
 
         $categories = Category::get();
-        $threads = Thread::latest()->withCount('replies')->get();
+        
+        // CONSULTA: 
+        $threads = Thread::query();
+        $threads->where('title', 'like', "%$this->search%"); 
+
+        if ($this->category) {
+            $threads->where('category_id', $this->category);
+        }
+
+        $threads->withCount('replies');
+        $threads->latest();
+        //dd($threads);
 
         return view('livewire.show-threads', [
             'categories' => $categories,
-            'threads' => $threads,
+            'threads' => $threads->get(),
         ]);
     }
 }
