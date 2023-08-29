006a90608a8da8424b6a6134f85fc396531c9316
Luis Anillo <luis1104anillo@gmail.com>
2023-08-29 12:13:42 -0500

    fixed the error

--- resources/views/thread/edit.blade.php
+++ resources/views/thread/edit.blade.php
@@ -6,7 +6,7 @@
                         Editar pregunta
                     </h2>
 
-                    <form action="{{ route('threads.update', 'thread') }}" method="POST">
+                    <form action="{{ route('threads.update', $thread) }}" method="POST">
                         @csrf
                         @method('PUT')
 
@@ -17,7 +17,6 @@
                             value="Editar pregunta"
                             class="w-full py-4 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-r hover:from-purple-500 hover:to-pink-500  text-white/80 text-xs font-bold rounded-md text-center"
                         >
-
                     </form>
             </div>
         </div>
